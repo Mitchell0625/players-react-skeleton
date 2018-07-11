d7d495d3d448073d356cef8d2b88aa465412508f
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-07-11 10:18:04 -0400

    added background-color; styled Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import '../styles/Header.css';
+import paddle from '../images/ping-pong.png';
 
-const Header = () => <div>Header</div>;
+const Header = () => (
+  <div className="__Header">
+    <h1 className="__Header_h1">Pongtastic</h1>
+    <img className="__Header_img" src={paddle} alt="ping pong paddle" />
+  </div>
+);
 
 export default Header;
